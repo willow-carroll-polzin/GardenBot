6e2676c3af3d041e36325e43be317cb057adb7e2
Zhang Xiaoran <cindy-zxr@hotmail.com>
2020-03-03 15:47:28 -0500

    sensor

--- gardenBot_actuation/Tester1/Tester1.ino
+++ gardenBot_actuation/Tester1/Tester1.ino
@@ -16,6 +16,16 @@ int valve3 = 3; //Valve 3
 int flag = 0; //0=no watering needed, 1=watering needed
 int curSensor[9]; //Current sesnors being read/watered
 
+//Sensor parameter:
+double WaterAna;
+double WaterNum;
+define Water_Level_Sensor A3
+
+double MoisAna[3];
+double MoisNum[3];
+static const uint8_t MoisPins[3] = {A0,A1,A2};
+
+
 //Actuation control:
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();  //Motor shield - Adafruit V2.0
 
@@ -53,8 +63,18 @@ void loop() {
       //TODO: Check moisture sensors (i.e. curSensor[i])
       //float curVal = analogRead(c0)*(5.0/1023.0);
       //Serial.println(curVal);
-      Serial.println("Checking moisture sensors...");
+ 
+     MoisAna[i] = analogRead(MoisPins[i]);
+     MoisNum[i] = ((590-MoisAna[i])/300)*100;
 
+     Serial.print("Line ");
+     Serial.print(i+1);
+     Serial.print(" - Moisture precent is:");
+     Serial.print(MoisNum[i]);
+     Serial.println("%");
+  
+     Serial.println("Checking moisture sensors...");
+      
       //IF CURSENSOR NEEDS WATER:
       if (checkWater > 0) {
         Serial.println("BEGIN:  WATERING!");
@@ -74,6 +94,14 @@ void loop() {
 //Read water level sensor
 int checkWater() {
   //TODO: Add code to check water level
+  WaterAna=(long)analogRead(Water_Level_Sensor);
+  
+  if(WaterAna>100){
+      return(1);
+  }else{
+      return(0);
+  }
+
   return (1); //Will return 1 if have enough water, else will return 0
 }
 
