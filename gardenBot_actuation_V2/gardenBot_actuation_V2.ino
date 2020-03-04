742eb12062bc48af994af0bc61234cb9da9aff2f
himMp09125511!? <max.polzin.5@gmail.com>
2020-03-03 21:14:30 -0500

    Merged sensing and actuation

--- gardenBot_actuation_V2/gardenBot_actuation_V2.ino
+++ gardenBot_actuation_V2/gardenBot_actuation_V2.ino
@@ -6,15 +6,24 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 //*************************
-//Irrigation control:
-int pump1 = 0; //Pump control pin
-int valve1 = 1; //Valve 1
-int valve2 = 2; //Valve 2
-int valve3 = 3; //Valve 3
+//Irrigation controls:
+#define pump1 0;              //Pump control pin
+#define valve1 1;             //Valve 1
+#define valve2 2;             //Valve 2
+#define valve3 3;             //Valve 3
+#define Water_Level_Sensor A3 //Water level sensor
+#define LED 8                 //Watering indicator LED
+
+//Watering sensor variables:
+double WaterAna;
+double WaterNum;
+double MoisAna[3];
+double MoisNum[3];
+static const uint8_t MoisPins[3] = {A0,A1,A2};
 
 //Sensor flag:
-int flag = 0; //0=no watering needed, 1=watering needed
 int curSensor[9] = {1,2,3,4,5,6,7,8,9}; //Current sesnors being read/watered
+int drySenesors[9]; //List of sensors that require water
 
 //Actuation control:
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();  //Motor shield - Adafruit V2.0
@@ -29,7 +38,9 @@ void setup() {
   pinMode(valve1, OUTPUT);
   pinMode(valve2, OUTPUT);
   pinMode(valve3, OUTPUT);
+  pinMode(LED,OUTPUT);
 
+  digitalWrite(pump1, LOW);
   digitalWrite(pump1, LOW);
   digitalWrite(valve1, LOW);
   digitalWrite(valve2, LOW);
@@ -49,37 +60,77 @@ void setup() {
 //*************************
 //Sensing and actuation loop:
 void loop() {
-    for (int i=0; i<9; i++) {
-      //TODO: Check moisture sensors (i.e. curSensor[i])
-      //float curVal = analogRead(c0)*(5.0/1023.0);
-      //Serial.println(curVal);
+    
+      //Check water sensors
       Serial.println("Checking moisture sensors...");
-
-      //IF CURSENSOR NEEDS WATER:
-      if (checkWater > 0) {
-        Serial.println("BEGIN:  WATERING!");
-        waterPlants(curSensor[i]); //Start watering routine, for current sensor
-      } else {
+      drySenesors = senseWater();
+      
+      //Check if there is water available
+      if (waterLevelDetection() == 1) {
+        //Cycle through all sensors that may need water
+        for (int i=0; i<sizeof(drySenesors); i++) {
+          //Check if the current sensor needs water
+          if (drySensors[i] != 0) { 
+            Serial.print("BEGIN WATERING: ");
+            Serial.println(curSensor[i]);
+            waterPlants(curSensor[i]); //Start watering routine, for current sensor
+            drySensor[i] = 0; //Reset the status of current sensor
+          }
+        }
+      } else if (waterLevelDetection() == 0) {
         Serial.println("ERROR: FILL RESERVOIR!");
-        //TODO: Send error message to GUI, stop all functions and wait
       }
-    }
-
-    flag = 1; //TEST
-    while (flag>0) {
-
-    }
 }
 //*************************
-//Read water level sensor
-int checkWater() {
-  //TODO: Add code to check water level
+//Read water level sensors
+int senseWater() {
+  for(int i = 0; i<9; i++) { 
+    MoisAna[i] = analogRead(MoisPins[i]);
+    MoisNum[i] = ((590-MoisAna[i])/300)*100;
+    
+    Serial.print("Line ");
+    Serial.print(i+1);
+    Serial.print(" - Moisture precent is:");
+    Serial.print(MoisNum[i]);
+    Serial.println("%");
+
+    //TODO: SET CORRECT MOISTURE THRESHOLD
+    int drySensors[9];
+    if MoisNum[i] < 10 {
+      drySensors[i] = 1;
+    } else {
+      drySensors[i] = 0;
+    }
+  }
+  return drySensors; //Will return list of sensors that require watering
+}
+
+//Check water level available
+int waterLevelDetection() {
+  WaterAna=(long)analogRead(Water_Level_Sensor);
+  WaterNum=(WaterAna/650)*4;
+  
+  Serial.print("the depth is:");
+  Serial.print(WaterNum);
+  Serial.println("cm");
+  
+  if(WaterAna<100){
+    digitalWrite(LED,HIGH);
+    //TODO: TURN OFF PUMP, ALERT USER
+    Serial.println("ERROR: WATER LEVEL TO LOW, REFILL REQUIRED!");
+  } else {
+    digitalWrite(LED,LOW);
+  }
+  
+  delay(1000);
   return (1); //Will return 1 if have enough water, else will return 0
 }
 
 //Watering routine
 void waterPlants(int curSensor) {
         //Actuate motors
+        //M3_L = RED , M3_R = GREEN
+        //M4_L = YELLOW , M4_R = BLUE 
         Serial.println("Moving to destination...");
         if (curSensor == 1 || curSensor == 2 || curSensor == 3) {
           myMotor->step(200,FORWARD, SINGLE); //Move to correct position
@@ -123,6 +174,6 @@ void waterPlants(int curSensor) {
         digitalWrite(valve1, LOW);
         digitalWrite(valve2, LOW);
         digitalWrite(valve3, LOW);
-        Serial.println("Vales closed.")
+        Serial.println("Vales closed.");
 }
 //*************************
